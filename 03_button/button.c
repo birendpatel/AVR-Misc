a96eeee5f1054606940f1b04b6f85bd38f0e331b
birendpatel <biren.dilip.patel@gmail.com>
2021-05-16 22:19:51 +0100

    disable builtin led

--- 03_button/button.c
+++ 03_button/button.c
@@ -8,15 +8,27 @@
 #include <avr/io.h>
 
 /*******************************************************************************
- * setup() - configure registers
+ * setup() - configure registers on reset
  * @PD2: output, drive low
  * @PD3: input, engage pull up resistor
+ * @PB5: output, drive low (builtin LED on arduino uno)
  ******************************************************************************/
 void setup(void) {
+        /*
+        No need to set PORTD2 to 0. For ATmega328p the datasheet specifies
+        that it already has an initial value of 0. When we configure the pin
+        for output, it drives low by default.
+        */
         DDRD = 1 << DDD2;
         PORTD = 1 << PORTD3;
 
-        return;
+        /*
+        Specifically for the arudino uno, the builtin LED defaults to input
+        with the pull up resistor deactivated. But the op-amp buffer can power
+        this LED. I choose to turn it off. In the future, we can toggle the
+        LED to signal program states for debugging.
+        */
+        DDRB = 1 << DDB5;
 }
 
 /*******************************************************************************
@@ -31,13 +43,14 @@ int main(void) {
                 setup, we enabled the pull up resistor on PD3. Therefore, 5V is
                 presented to the input pin by default. If the button is pressed,
                 0V is presented, and therefore the PINx register will read zero.
-                This cues PD2 to branch and output 5V.
+                This cues PD2 to branch and output 5V. Note, the sfr_defs header
+                provides the bit_is_clear() macro to encapsulate the read op.
                 */
                 if ((PIND & (1 << PIND3)) == false) {
                         PORTD |= 1 << PORTD2;
                 }
 
-                PORTD |= 0 << PORTD2;
+                PORTD &= ~(1 << PORTD2);
         }
 
         return 0;
