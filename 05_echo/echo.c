a62717d0359bfb0b310c00c6d3ac0fe750e60dff
birendpatel <biren.dilip.patel@gmail.com>
2021-05-20 15:56:25 +0100

    add schematic and spec

---
+++ 05_echo/echo.c
@@ -0,0 +1,18 @@
+/*
+* Copyright (C) 2021 Biren Patel
+* MIT License
+*/
+
+#ifndef F_CPU
+        #error "F_CPU not defined"
+#endif
+
+#ifndef BAUD_RATE
+        #error "BAUD_RATE not defined"
+#endif
+
+#include <avr/io.h>
+
+int main(void) {
+        return 0;
+}
