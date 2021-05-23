81638cb2236c7f6594dd69f1c266560cabf0b6c0
birendpatel <biren.dilip.patel@gmail.com>
2021-05-23 23:45:02 +0100

    add deque_new tests

--- assets/test_deque.c
+++ assets/test_deque.c
@@ -4,14 +4,54 @@
 * Unit tests for double ended queue
 */
 
-#include <stdlib.h>
+#include <stdint.h>
 
 #include "deque.h"
 #include "unity.h"
 
+void test_new_deque_zero_capacity_returns_error(void) {
+        //arrange
+        struct deque dq;
+        uint8_t err;
+        unsigned char buf[] = {1, 2, 3};
+
+        //act
+        err = deque_new(&dq, buf, 0);
+
+        //assert
+        TEST_ASSERT_EQUAL(DEQUE_CAP_BOUNDS, err);
+}
+
+void test_new_deque_null_buffer_returns_error(void) {
+        //arrange
+        struct deque dq;
+        uint8_t err;
+
+        //act
+        err = deque_new(&dq, NULL, 1);
+
+        //assert
+        TEST_ASSERT_EQUAL(DEQUE_NULL_INPUT, err);
+}
+
+void test_new_deque_with_legal_parameters_returns_success(void) {
+        //arrange
+        struct deque dq;
+        uint8_t err;
+        unsigned char buf[] = {1, 2, 3};
+
+        //act
+        err = deque_new(&dq, buf, sizeof(buf));
+
+        //assert
+        TEST_ASSERT_EQUAL(DEQUE_SUCCESS, err);
+}
+
 int main(void)
 {
         UNITY_BEGIN();
-
+                RUN_TEST(test_new_deque_zero_capacity_returns_error);
+                RUN_TEST(test_new_deque_null_buffer_returns_error);
+                RUN_TEST(test_new_deque_with_legal_parameters_returns_success);
         return UNITY_END();
 }
