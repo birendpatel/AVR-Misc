f65f65be35356cbe5bff64e8dabb9582bbd0f0ce
birendpatel <biren.dilip.patel@gmail.com>
2021-06-04 18:06:25 +0100

    start dio driver

---
+++ drivers/dio.c
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * @file: dio.c
+ * @brief: Implementation of digital input/ouput driver for ATmega328P
+ * @author: Copyright (C) 2021 Biren Patel. This project is released under the
+ * MIT License.
+ ******************************************************************************/
+
+ #include <avr/io.h>
+ #include <avr/pgmspace.h>
+
+ #include "dio.h"
