1d930b093c6532a56224c88b82b00112dbe85281
birendpatel <biren.dilip.patel@gmail.com>
2021-06-05 15:00:45 +0100

    stabilized dio api

--- drivers/dio.c
+++ drivers/dio.c
@@ -1,16 +1,11 @@
 /*******************************************************************************
  * @file dio.c
  * @brief Implementation of digital input/ouput driver for ATmega328P
- * @author Copyright (C) 2021 Biren Patel. This project is released under the
- * MIT License.
+ * @author Copyright (C) 2021 Biren Patel.
+ * @license This project is released under the MIT License.
  ******************************************************************************/
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 
 #include "dio.h"
-
-int main(void) {
-        dio_open(PIN24, OUTPUT | HIGH);
-        dio_open(PIN17, INPUT | PULLUP);
-}
--- drivers/dio.h
+++ drivers/dio.h
@@ -1,9 +1,9 @@
 /*******************************************************************************
 * @file dio.h
-* @brief Digital input/ouput driver for ATmega328P
-* @author Copyright (C) 2021 Biren Patel. This project is released under the
-* MIT License.
-* @details dio.h is a driver which encapsulates digital I/O operations similar
+* @brief Digital input/ouput driver for ATmega328P with SPDIP configuration
+* @author Copyright (C) 2021 Biren Patel.
+* @license This project is released under the MIT License.
+* @details DIO is a driver which encapsulates digital I/O operations similar
 * to the arduino platform. For example, digitalWrite(3, HIGH) on the arduino
 * translates to dio_write(PIN3, HIGH) within the dio.h API.
 *******************************************************************************/
@@ -11,52 +11,69 @@
 #ifndef DIO_H
 #define DIO_H
 
-/*******************************************************************************
-* @def no_address
-* @brief The no_address macro is a synonym for the C register keyword
-* @details Why would anyone use the register keyword in the 21st century? Am I
-* just a crazy fool? Maybe! I use register as a compile-time check. It's like
-* the const keyword. Const offers an optimization benefit but it also lets the
-* compiler inform me if the read-only contract was violated. Register does NOT
-* offer an optimization benefit. However, it lets the compiler inform me if the
-* local-scope no-references contract of a certain variable was violated. YMMV.
-*******************************************************************************/
-#ifndef no_address
-        #define no_address register
-#endif
+/* error codes */
+#define DIO_SUCCESS     0 /**< @brief function returned successfully          */
+#define DIO_LOOKUP      1 /**< @brief input pin argument does not exist       */
+#define DIO_MODE        2 /**< @brief input mode argument is invalid          */
+#define DIO_VALUE       3 /**< @brief input value argument is invalid         */
+#define DIO_PULLUP      4 /**< @brief cannot engage pullup on output pin      */
 
-/*******************************************************************************
-* @def PIN0
-* @brief Digital I/O pin map
-* @details Every pin on the ATmega328P that may be configured for digital I/O
-* has a corresponding pin macro. The user uses these pin macros to interface
-* with the dio_* functions instead of performing operations directly on the
-* DDRx, PORTx, and PINx registers.
-*******************************************************************************/
-#define PIN0    (uint8_t) 255   /**< NUL */
-#define PIN1    (uint8_t) 0     /**< PC6 */
-#define PIN2    (uint8_t) 1     /**< PD0 */
-#define PIN3    (uint8_t) 2     /**< PD1 */
-#define PIN4    (uint8_t) 3     /**< PD2 */
-#define PIN5    (uint8_t) 4     /**< PD3 */
-#define PIN6    (uint8_t) 5     /**< PD4 */
-#define PIN9    (uint8_t) 6     /**< PB6 */
-#define PIN10   (uint8_t) 7     /**< PB7 */
-#define PIN11   (uint8_t) 8     /**< PD5 */
-#define PIN12   (uint8_t) 9     /**< PD6 */
-#define PIN13   (uint8_t) 10    /**< PD7 */
-#define PIN14   (uint8_t) 11    /**< PB0 */
-#define PIN15   (uint8_t) 12    /**< PB1 */
-#define PIN16   (uint8_t) 13    /**< PB2 */
-#define PIN17   (uint8_t) 14    /**< PB3 */
-#define PIN18   (uint8_t) 15    /**< PB4 */
-#define PIN19   (uint8_t) 16    /**< PB5 */
-#define PIN23   (uint8_t) 17    /**< PC0 */
-#define PIN24   (uint8_t) 18    /**< PC1 */
-#define PIN25   (uint8_t) 19    /**< PC2 */
-#define PIN26   (uint8_t) 20    /**< PC3 */
-#define PIN27   (uint8_t) 21    /**< PC4 */
-#define PIN28   (uint8_t) 22    /**< PC5 */
+/* api mode and value args */
+#define INPUT   0
+#define OUTPUT  1
+#define LOW     0
+#define HIGH    1
+#define PULLUP  2
+
+/* map: SPDIP physical layout to pin configurations */
+#define PB0 (uint8_t) 0
+#define PB1 (uint8_t) 1
+#define PB2 (uint8_t) 2
+#define PB3 (uint8_t) 3
+#define PB4 (uint8_t) 4
+#define PB5 (uint8_t) 5
+#define PB6 (uint8_t) 6
+#define PB7 (uint8_t) 7
+#define PC0 (uint8_t) 8
+#define PC1 (uint8_t) 9
+#define PC2 (uint8_t) 10
+#define PC3 (uint8_t) 11
+#define PC4 (uint8_t) 12
+#define PC5 (uint8_t) 12
+#define PC6 (uint8_t) 13
+#define PD0 (uint8_t) 14
+#define PD1 (uint8_t) 15
+#define PD2 (uint8_t) 16
+#define PD3 (uint8_t) 17
+#define PD4 (uint8_t) 18
+#define PD5 (uint8_t) 19
+#define PD6 (uint8_t) 20
+#define PD7 (uint8_t) 21
+
+/* map: pin configurations to digital I/O pin interface */
+#define PIN1    PC6
+#define PIN2    PD0
+#define PIN3    PD1
+#define PIN4    PD2
+#define PIN5    PD3
+#define PIN6    PD4
+#define PIN9    PB6
+#define PIN10   PB7
+#define PIN11   PD5
+#define PIN12   PD6
+#define PIN13   PD7
+#define PIN14   PB0
+#define PIN15   PB1
+#define PIN16   PB2
+#define PIN17   PB3
+#define PIN18   PB4
+#define PIN19   PB5
+#define PIN23   PC0
+#define PIN24   PC1
+#define PIN25   PC2
+#define PIN26   PC3
+#define PIN27   PC4
+#define PIN28   PC5
 
 /*******************************************************************************
 * @function dio_open
@@ -64,13 +81,7 @@
 * @param[in] pin One of PIN1 thru PIN28
 * @param[in] mode Either INPUT or OUTPUT
 *******************************************************************************/
-#define INPUT   0
-#define OUTPUT  1
-
-uint8_t dio_open (
-        no_address const uint8_t pin,
-        no_address const uint8_t mode
-);
+uint8_t dio_open(uint8_t pin, uint8_t mode);
 
 /*******************************************************************************
 * @function dio_write
@@ -78,13 +89,22 @@ uint8_t dio_open (
 * @param[in] pin One of PIN1 thru PIN28
 * @param[in] value One of HIGH, LOW, or PULLUP
 *******************************************************************************/
-#define HIGH    1
-#define LOW     0
-#define PULLUP  1
+uint8_t dio_write(uint8_t pin, uint8_t value);
 
-uint8_t dio_write (
-        no_address const uint8_t pin,
-        no_address const uint8_t value
-);
+/*******************************************************************************
+* @function dio_read
+* @brief Read voltage on an input pin
+* @param[in] pin One of PIN1 thru PIN28
+* @param[out] value Either HIGH or LOW on successful return
+*******************************************************************************/
+uint8_t dio_read(uint8_t pin, uint8_t *value);
+
+/*******************************************************************************
+* @function dio_toggle
+* @brief Toggle a pin from HIGH to LOW or from LOW to HIGH
+* @param[in] pin One of PIN1 thru PIN28
+* @param[out] value Contains new pin status as either HIGH or LOW. May be NULL.
+*******************************************************************************/
+uint8_t dio_toggle(uint8_t pin, uint8_t *value);
 
 #endif /* DIO_H */
