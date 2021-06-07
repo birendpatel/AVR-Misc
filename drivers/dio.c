cd5472bf200b8e1ae23021ec73e860536014c319
birendpatel <biren.dilip.patel@gmail.com>
2021-06-07 14:03:02 +0100

    make dio_open atomic

--- drivers/dio.c
+++ drivers/dio.c
@@ -5,8 +5,10 @@
 * @license This project is released under the MIT License.
 *******************************************************************************/
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+
 #include "dio.h"
 
 static uint8_t dio_open_entry(const uint8_t pin, const uint8_t mode);
@@ -66,10 +68,25 @@ static const attributes lookup[] PROGMEM = {
         [DIO_D7] = { &PIND, 1 << PIND7, PIND7 },
 };
 
-/******************************************************************************/
+/*******************************************************************************
+dio_open ensures that the DDR and PORT assignments happen atomically. We don't
+want an interrupt to occur after the DDR assignment and before the PORT
+assignment. These are edge scenarios where an interrupt doesn't use the DIO
+interface and attempts to modify the same DDR as held in the current iteration.
+
+The following path is unusual but has the unintended effect of engaging the D0
+pullup resistor.
+
+1. dio_open_entry() -> DDRD |= (1 << DDD0)
+2. interrupt -> DDRD &= ~ (1 << DDD0)
+3. dio_write() -> PORTD |= (1 << PORTD0)
+*/
 
 uint8_t dio_open(const dio_config *const table, const uint8_t n)
 {
+        uint8_t err = 0;
+        uint8_t gei = 0;
+
         if (!table) {
                 return DIO_ERR_NULL;
         }
@@ -79,23 +96,33 @@ uint8_t dio_open(const dio_config *const table, const uint8_t n)
         }
 
         for (uint8_t i = 0; i < n; i++) {
-                uint8_t err = 0;
                 dio_config entry = table[i];
+
+                /* an ISR may execute between these instructions */
+
+                gei = SREG >> 7;
+                cli();
 
                 err = dio_open_entry(entry.pin, entry.mode);
 
                 if (err) {
-                        return err;
+                        goto fail;
                 }
 
                 err = dio_write(entry.pin, entry.value);
 
                 if (err) {
-                        return err;
+                        goto fail;
                 }
+
+                SREG |= (gei << 7);
         }
 
         return DIO_SUCCESS;
+
+fail:
+        SREG |= (gei << 7);
+        return err;
 }
 
 /******************************************************************************/
@@ -141,16 +168,16 @@ uint8_t dio_write(const uint8_t pin, const uint8_t value)
         volatile uint8_t *const port_reg = attr.pin_reg + 2;
 
         switch (value) {
-                case TOGGLE:
-                        *port_reg ^= attr.mask;
-                        break;
-
                 case LOW:
                         *port_reg &= ~(attr.mask);
                         break;
 
                 case HIGH: /* PULLUP on INPUT */
                         *port_reg |= attr.mask;
+                        break;
+
+                case TOGGLE:
+                        *port_reg ^= attr.mask;
                         break;
 
                 default:
